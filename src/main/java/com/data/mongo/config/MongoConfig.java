c30316f26c220c92aedc1d962be387b12b636dc9
BhupinderS24 <ikwvibe1@gmail.com>
2019-03-15 19:50:46 +0530

    API using Mongodb

--- src/main/java/com/data/mongo/config/MongoConfig.java
+++ src/main/java/com/data/mongo/config/MongoConfig.java
@@ -1,16 +1,10 @@
 package com.data.mongo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.expression.EnvironmentAccessor;
 import org.springframework.core.env.Environment;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
 
 @Configuration
 @PropertySource("classpath:mongo.properties")
